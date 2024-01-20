4da6896c445b8f5b69285122d476318507779ebe
Salim Nassim <salim.nassim@gmail.com>
2024-01-20 14:53:29 -0500

    separate helpers

---
+++ src/types/helpers.ts
@@ -0,0 +1,31 @@
+export function priority(priority: number): string {
+  switch (priority) {
+    case 0:
+      return 'Off'
+    case 1:
+      return 'Low'
+    case 2:
+      return 'Normal'
+    case 3:
+      return 'High'
+    default:
+      return '?'
+  }
+}
+
+export function percent(size_bytes: number, bytes_completed: number): string {
+  const percent = (size_bytes / bytes_completed) * 100
+  if (percent === Infinity) {
+    return '∞'
+  }
+  return `${percent}%`
+}
+
+// Converts bytes to a human readable format.
+export function human(bytes: number) {
+  if (bytes === undefined || bytes === 0) {
+    return '0'
+  }
+  const index = Math.floor(Math.log(bytes) / Math.log(1024))
+  return `${Number((bytes / Math.pow(1024, index)).toFixed(2)) * 1} ${(['B', 'KB', 'MB', 'GB', 'TB'] as const)[index]}`
+}
