285c27751d16403b76c9b814eea71154de674c71
Salim Nassim <salim.nassim@gmail.com>
2024-01-20 15:30:53 -0500

    add expander tab group

--- src/components/TorrentExpander.vue
+++ src/components/TorrentExpander.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>expander</div>
+    <div class="flex flex-col h-full border-t border-slate-800 p-2">
+        <div class="flex justify-between">
+            <div>{{ expander.torrent?.name }}</div>
+            <XCircleIcon @click="$emit('close')" class="h-6 w-6 hover:cursor-pointer" />
+        </div>
+        <TorrentExpanderTabGroup />
+        <div class="h-full overflow-y-auto"></div>
+    </div>
 </template>
 
 <script setup lang="ts">
 import { type Expander } from '@/stores/store'
+import { XCircleIcon } from '@heroicons/vue/24/solid'
+import TorrentExpanderTabGroup from '@/components/TorrentExpanderTabGroup.vue'
 defineProps<{
     expander: Expander
 }>()
---
+++ src/components/TorrentExpanderTabGroup.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-row text-sm mt-2 border-b border-slate-600">
+        <div class="bg-slate-600 mr-2 p-1 hover:cursor-pointer select-none">Details</div>
+        <div class="bg-slate-600 mr-2 p-1 hover:cursor-pointer select-none">Files</div>
+        <div class="bg-slate-600 mr-2 p-1 hover:cursor-pointer select-none">Trackers</div>
+        <div class="bg-slate-600 mr-2 p-1 hover:cursor-pointer select-none">Peers</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineEmits<{
+    close: [void]
+}>()
+</script>
